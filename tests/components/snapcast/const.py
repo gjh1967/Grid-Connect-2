1ad924eb7729a35b34713ae4ab01c5186203a8f2
gjh1967 <44850142+gjh1967@users.noreply.github.com>
2025-12-02 06:30:35 +0000

    Final cleanup of Home Assistant Core development files

--- tests/components/snapcast/const.py
+++
@@ -1,4 +0,0 @@
-"""Constants for Snapcast tests."""
-
-TEST_CLIENT_ENTITY_ID = "media_player.test_client_snapcast_client"
-TEST_GROUP_ENTITY_ID = "media_player.test_group_snapcast_group"
--- tests/grid_connect/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Tests for the Grid connect integration."""
